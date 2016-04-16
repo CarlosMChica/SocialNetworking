8fc0d5c36e5aad03fd653910e7ea2767e9021cb6
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-04-17 00:08:15 +0100

    Extract constant

--- src/main/java/carlosdelachica/console/InputParser.java
+++ src/main/java/carlosdelachica/console/InputParser.java
@@ -3,8 +3,11 @@ package carlosdelachica.console;
 import static carlosdelachica.console.Input.Type.POST;
 
 public class InputParser {
+
+  private static final String POST_ACTION = " -> ";
+
   public Input parse(String input) {
-    String[] arguments = input.split(" -> ");
+    String[] arguments = input.split(POST_ACTION);
     return new Input(POST, arguments);
   }
 }
