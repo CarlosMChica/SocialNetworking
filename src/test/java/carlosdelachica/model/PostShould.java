5cca8b46ba3c6a24bef7a5e7219f76e394dc0cd2
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-04-17 15:56:25 +0100

    Implement is post from user

---
+++ src/test/java/carlosdelachica/model/PostShould.java
@@ -0,0 +1,35 @@
+package carlosdelachica.model;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class PostShould {
+
+  private static final String USER_NAME = "userName";
+  private static final String OTHER_USER_NAME = "otherUserName";
+  private static final User USER = new User(USER_NAME);
+  private static final User OTHER_USER = new User(OTHER_USER_NAME);
+
+  private static final long ANY_TIMESTAMP = 0L;
+  private static final String ANY_MESSAGE = "";
+
+  @Test public void return_is_from_user_for_user_post() {
+    Post post = new Post(USER, ANY_MESSAGE, ANY_TIMESTAMP);
+
+    boolean userPost = post.isFrom(USER);
+
+    assertThat(userPost, is(true));
+  }
+
+  @Test public void return_is_not_from_user_for_other_user_post() {
+    Post post = new Post(USER, ANY_MESSAGE, ANY_TIMESTAMP);
+
+    boolean userPost = post.isFrom(OTHER_USER);
+
+    assertThat(userPost, is(false));
+
+
+  }
+}
