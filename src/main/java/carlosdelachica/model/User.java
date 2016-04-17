aafa922b83ea3c0c6c8a1537ec17d040f42f7e55
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-04-17 21:03:46 +0100

    Add support to store user friends

--- src/main/java/carlosdelachica/model/User.java
+++ src/main/java/carlosdelachica/model/User.java
@@ -1,15 +1,26 @@
 package carlosdelachica.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.unmodifiableList;
+
 public class User {
 
   private final String name;
+  private List<User> friends;
 
   public User(String name) {
     this.name = name;
+    this.friends = new ArrayList<>();
   }
 
   public void follow(User friend) {
-    throw new UnsupportedOperationException();
+    friends.add(friend);
+  }
+
+  public List<User> friends() {
+    return unmodifiableList(friends);
   }
 
   @Override public String toString() {
---
+++ src/test/java/carlosdelachica/model/UserShould.java
@@ -0,0 +1,23 @@
+package carlosdelachica.model;
+
+import java.util.List;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class UserShould {
+
+  private static final String ANY_USER_NAME = "userName";
+  private static final User FRIEND = new User("friendName");
+
+  @Test public void store_user_friends() {
+    User user = new User(ANY_USER_NAME);
+
+    user.follow(FRIEND);
+
+    List<User> friends = user.friends();
+    assertThat(friends.size(), is(1));
+    assertThat(friends.get(0), is(FRIEND));
+  }
+}
