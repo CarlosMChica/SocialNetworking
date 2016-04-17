826999093700be23bbb3a7305b22f55bcc42e750
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-04-17 18:12:16 +0100

    Implement input parsing for reading command

--- src/main/java/carlosdelachica/delivery_mechanism/InputParser.java
+++ src/main/java/carlosdelachica/delivery_mechanism/InputParser.java
@@ -3,13 +3,25 @@ package carlosdelachica.delivery_mechanism;
 import carlosdelachica.model.Input;
 
 import static carlosdelachica.model.Input.Type.POST;
+import static carlosdelachica.model.Input.Type.READ;
 
 public class InputParser {
 
   private static final String POST_ACTION = " -> ";
 
   public Input parse(String input) {
-    String[] arguments = input.split(POST_ACTION);
-    return new Input(POST, arguments);
+    if (isPostType(input)) {
+      return new Input(POST, argumentsFor(POST_ACTION, input));
+    } else {
+      return new Input(READ, input);
+    }
+  }
+
+  private boolean isPostType(String input) {
+    return argumentsFor(POST_ACTION, input).length > 1;
+  }
+
+  private String[] argumentsFor(String action, String input) {
+    return input.split(action);
   }
 }
--- src/test/java/carlosdelachica/delivery_mechanism/InputParserShould.java
+++ src/test/java/carlosdelachica/delivery_mechanism/InputParserShould.java
@@ -5,14 +5,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static carlosdelachica.model.Input.Type.POST;
+import static carlosdelachica.model.Input.Type.READ;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class InputParserShould {
 
-  private static final String FIRST_ARGUMENT = "arg1";
-  private static final String SECOND_ARGUMENT = "arg2";
-  private static final String[] ARGUMENTS = new String[] {FIRST_ARGUMENT, SECOND_ARGUMENT};
+  private static final String READ_ARGUMENT = "readArgument";
+  private static final String POST_FIRST_ARGUMENT = "arg1";
+  private static final String POST_SECOND_ARGUMENT = "arg2";
+  private static final String[] ARGUMENTS =
+      new String[] {POST_FIRST_ARGUMENT, POST_SECOND_ARGUMENT};
 
   private InputParser parser;
 
@@ -29,7 +32,20 @@ public class InputParserShould {
     assertThat(parsedInput, is(expectedInput));
   }
 
+  @Test public void parse_input_with_read_action() {
+    String input = givenInputWithReadAction();
+
+    Input parsedInput = parser.parse(input);
+
+    Input expectedInput = new Input(READ, READ_ARGUMENT);
+    assertThat(parsedInput, is(expectedInput));
+  }
+
+  private String givenInputWithReadAction() {
+    return READ_ARGUMENT;
+  }
+
   private String givenInputWithPostAction() {
-    return FIRST_ARGUMENT + " -> " + SECOND_ARGUMENT;
+    return POST_FIRST_ARGUMENT + " -> " + POST_SECOND_ARGUMENT;
   }
 }
