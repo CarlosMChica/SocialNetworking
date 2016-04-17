834faad4857470eec5d1d4142d49a4163bf0badb
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-04-17 12:53:19 +0100

    Rename Console to ConsoleWrapper

---
+++ src/main/java/carlosdelachica/console/ConsoleWrapper.java
@@ -0,0 +1,16 @@
+package carlosdelachica.console;
+
+import java.util.Scanner;
+
+public class ConsoleWrapper {
+
+  private Scanner scanner = new Scanner(System.in);
+
+  String readLine() {
+    return scanner.nextLine();
+  }
+
+  public void print(String line) {
+    System.out.print(line);
+  }
+}
