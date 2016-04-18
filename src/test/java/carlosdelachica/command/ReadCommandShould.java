a92e38a10b350d11118a2b1526d9a8d97740aa2c
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-04-18 18:54:37 +0100

    Rename posts instance variables

--- src/test/java/carlosdelachica/command/ReadCommandShould.java
+++ src/test/java/carlosdelachica/command/ReadCommandShould.java
@@ -25,11 +25,11 @@ import static org.mockito.Mockito.*;
   private static final String POST_2_MESSAGE = "SecondMessage";
   private static final long POST_1_TIMESTAMP = 0L;
   private static final long POST_2_TIMESTAMP = 1L;
-  private static final Post FIRST_POST = new Post(ANY_USER, POST_1_MESSAGE, POST_1_TIMESTAMP);
-  private static final Post SECOND_POST = new Post(ANY_USER, POST_2_MESSAGE, POST_2_TIMESTAMP);
+  private static final Post POST_1 = new Post(ANY_USER, POST_1_MESSAGE, POST_1_TIMESTAMP);
+  private static final Post POST_2 = new Post(ANY_USER, POST_2_MESSAGE, POST_2_TIMESTAMP);
 
-  private static final List<Post> USER_POSTS = asList(FIRST_POST, SECOND_POST);
-  private static final List<Post> SORTED_USER_POSTS = asList(SECOND_POST, FIRST_POST);
+  private static final List<Post> USER_POSTS = asList(POST_1, POST_2);
+  private static final List<Post> SORTED_USER_POSTS = asList(POST_2, POST_1);
 
   @Mock View view;
   @Mock PostRepository postRepository;
